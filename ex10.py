8702fbe50bcf0e1510a0c9dddfafe2f0dac03f92
Nelluri Saivaishnavi <nellurisaivaishnavi107@gmail.com>
2026-09-07 16:06:22 +0530

    multiple inheritance

---
+++ ex10.py
@@ -0,0 +1,13 @@
+class animal():
+  def eat(self):
+    print("animal eat")
+class animal_other():
+  def sleep(self):
+    print("animal_other sleep")
+class dog(animal,animal_other):
+  def walk(self):
+    print("dog walk")
+d1=dog()
+d1.eat()
+d1.sleep()
+d1.walk()
