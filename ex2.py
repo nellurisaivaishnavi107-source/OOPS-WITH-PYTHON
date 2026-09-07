ca9abd05b421e7bbdb46475ff6c92c0520f69cc8
Nelluri Saivaishnavi <nellurisaivaishnavi107@gmail.com>
2026-09-07 15:36:49 +0530

    create a class with multiple methods

---
+++ ex2.py
@@ -0,0 +1,11 @@
+class Demo():
+def addition(self,a,b):
+print(a+b)
+def subtraction(self,a,b):
+print(a-b)
+def multiply(self,a,b):
+print(a*b)
+d1=Demo()
+d1.addition(30,40)
+d1.subtraction(40,30)
+d1.multiply(10,3)
